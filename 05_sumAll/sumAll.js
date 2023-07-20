09c8898350228ae77294cbf7896276ca880e4f82
humbleodin <110336532+attentiondude132@users.noreply.github.com>
2023-07-19 19:18:34 -0700

    gives me the sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let output = 0;
+    if ((typeof a !== "number" || typeof b !== "number")) {
+        // (a returns true, b is false)
+        return "ERROR";
+    }
+    if (a > 0 && b > a) {
+        for (i = a; i <= b; i++) {
+            output = Number(output) + Number(i);
+            }
+    }
+    else if(a > b) {
+        for (i = a; i >= b; i--) {
+            output = Number(output) + Number(i);
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return output;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
