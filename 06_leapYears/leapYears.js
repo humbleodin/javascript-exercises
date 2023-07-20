b77bffb53c63b246f2212911a4a904e9a546f0f3
humbleodin <110336532+attentiondude132@users.noreply.github.com>
2023-07-19 20:07:13 -0700

    Add pseodocode bc I think it's important in my learning

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,3 +1,9 @@
+// create a function determing if given year is a leap year
+// leap years are divisible by 4 
+// leap years are not divisible by 100, unless also divisible by 400
+// if returns true, then it is leap year
+// false then is not a leap year
+
 const leapYears = function(year) {
     if (year % 100 === 0) {
         if (year % 400 === 0) {
