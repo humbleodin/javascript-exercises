c973345344940cec551199d3c07500dcc0a475f0
humbleodin <110336532+attentiondude132@users.noreply.github.com>
2023-07-19 20:54:41 -0700

    Convert from C to F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const convertToCelsius = function(temp) {
-  let celcius = (temp - 32) * (5 / 9)
-  return Math.round(celcius * 10) / 10;
+const convertToCelsius = function(tempF) {
+  let celcius = (tempF - 32) * (5 / 9);
+  // math.round(celcius * 10) will round celcius*10, then /10 on the outside to get 1 decimal place
+  return Math.round(celcius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let fahrenheit = (tempC) * 9 / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
+
 
 // Do not edit below this line
 module.exports = {
